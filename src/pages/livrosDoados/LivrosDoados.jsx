5aa822639589ed0dbae776af86376387652fa3e3
IsaacVNX <isaacmoura.c69@gmail.com>
2024-12-02 19:30:26 -0300

    fourth commit - Livros Vai na Web

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,9 +1,28 @@
 import s from './livrosDoados.module.scss'
+import livroProtagonista from '../../assets/livroProtagonista.png'
 
 export default function LivrosDoados() {
     return (
-        <section>
+        <section className={s.livroDoadosSection}>
             <h2>Livros Doados</h2>
+            <section className={s.containerCards}>
+                <section>
+                    <img src={livroProtagonista} alt="Imagem do livro O Protagonista" />
+                    <div>
+                        <h3>O Protagonista</h3>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                </section>
+                <section>
+                    <img src={livroProtagonista} alt="Imagem do livro O Protagonista" />
+                    <div>
+                        <h3>O Protagonista</h3>
+                        <p>Susanne Andrade</p>
+                        <p>Ficção</p>
+                    </div>
+                </section>
+            </section>
         </section >
     )
 }
