104db4b6ecc5f99e393b10f5f53d42b60349c637
IsaacVNX <isaacmoura.c69@gmail.com>
2025-04-07 21:47:23 -0300

    Livros Vai Na Web - SIte Completo

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,20 +1,34 @@
 import s from './livrosDoados.module.scss'
 import livroProtagonista from '../../assets/livroProtagonista.png'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function LivrosDoados() {
+
+    const [livros, setLivros] = useState([])
+
+    const puxarLivros = async () => {
+        const resposta = await axios.get("https://api-livros-vai-na-web-gehe.onrender.com/livros")
+        console.log(resposta)
+        setLivros(resposta.data)
+    }
+    useEffect(() => {
+        puxarLivros()
+    }, [])
+
     return (
-        <section className={s.livroDoadosSection}>
+        <section className={s.livrosDoadosSection}>
             <h2>Livros Doados</h2>
             <section className={s.containerCards}>
-                <section>
-                    <img src={livroProtagonista} alt="Imagem do livro O Protagonista" />
-                    <div>
-                        <h3>O Protagonista</h3>
-                        <p>Susanne Andrade</p>
-                        <p>Ficção</p>
-                    </div>
-                </section>
+                {livros.map((item) => (
+                    <section>
+                        <img src={item.image_url} alt={`Imagem do livro ${item.titulo}`} />
+                        <div>
+                            <h3>{item.titulo}</h3>
+                        </div>
+                    </section>
+                ))}
             </section>
-        </section >
+        </section>
     )
 }
--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,22 +1,55 @@
 import s from './queroDoar.module.scss'
 import iconeLivroAberto from '../../assets/iconeLivroAberto.png'
+import { useState } from 'react'
+import axios from 'axios'
 
 export default function QueroDoar() {
+
+    const [titulo,setTitulo] = useState("")
+    const [categoria,setCategoria] = useState("")
+    const [autor,setAutor] = useState("")
+    const [image_url,setImage_url] = useState("")
+
+    const capturaTitulo = (e) =>{
+        setTitulo(e.target.value)
+    }
+
+    const capturaCategoria = (e) =>{
+        setCategoria(e.target.value)
+    }
+
+    const capturaAutor = (e) =>{
+        setAutor(e.target.value)
+    }
+
+    const capturaImg = (e) =>{
+        setImage_url(e.target.value)
+    }
+
+    const envioDados = async() =>{
+
+        const dadosParaEnviar = {
+            titulo,
+            categoria,
+            autor,
+            image_url
+        }
+        await axios.post("https://api-livros-vai-na-web-gehe.onrender.com/doar",dadosParaEnviar)
+    }
     return (
         <section className={s.queroDoarSection}>
             <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
-            <form action="">
+            <form onSubmit={(e)=>e.preventDefault()}>
                 <div>
                     <img src={iconeLivroAberto} alt="imagem minimalista de livro aberto" />
-                    <p>Informações do Livro</p>
+                    <h2>Informações do Livro</h2>
                 </div>
-                <input type="text" name="" id="" placeholder='Titulo' />
-                <input type="text" name="" id="" placeholder='Categoria' />
-                <input type="text" name="" id="" placeholder='Autor' />
-                <input type="text" name="" id="" placeholder='Link da Imagem' />
-                <input type="submit" value="Doar" className={s.button}/>
+                <input type="text" placeholder='Título' onChange={capturaTitulo} required />
+                <input type="text" placeholder='Categoria' onChange={capturaCategoria} required /> 
+                <input type="text" placeholder='Autor' onChange={capturaAutor} required />
+                <input type="url" placeholder='Link da Imagem' onChange={capturaImg} required />
+                <input type="submit" value="Doar" className={s.buttonDoar} onClick={envioDados}/>
             </form>
         </section>
-
-    )
+        )
 }
